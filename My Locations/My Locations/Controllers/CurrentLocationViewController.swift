c42668333d384a50aa3dcac04d77b190134e1bfe
Abel Osorio <abel@kogimobile.com>
2016-02-17 09:02:06 -0500

    first commit changing file system structure

---
+++ My Locations/My Locations/Controllers/CurrentLocationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FirstViewController.swift
+//  My Locations
+//
+//  Created by Abel Osorio on 2/17/16.
+//  Copyright © 2016 Abel Osorio. All rights reserved.
+//
+
+import UIKit
+
+class CurrentLocationViewController: UIViewController {
+    
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var latitudeLabel: UILabel!
+    @IBOutlet weak var longitudeLabel: UILabel!
+    @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var tagButton: UIButton!
+    @IBOutlet weak var getButton: UIButton!
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+    
+}
+
